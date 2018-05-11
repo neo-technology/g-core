322293f6edbe862c2ee3f0aa7e2eb591e05ce6ba
Georgiana Diana <georgiana.ciocirdel@cwi.nl>
2018-05-11 14:10:13 +0200

    Fix missing id column problem.
    In Projection (actually AdColumn that uses Projection), check that a
    variable that already exists in the schema and is projected has an id
    column. If not, add it here. Variables without id can appear here as a
    result of GroupBy, where we add temporary aggregate properties.

--- src/main/scala/spark/sql/operators/Project.scala
+++ src/main/scala/spark/sql/operators/Project.scala
@@ -11,72 +11,71 @@ case class Project(relation: TargetTreeNode, attributes: Seq[Reference])
   extends target_api.Project(relation, attributes) {
 
   override val bindingTable: BindingTableMetadata = {
-    val relationBtable: SqlBindingTableMetadata = relation.bindingTable.asInstanceOf[SqlBindingTableMetadata]
-    val relationSchema: StructType = relationBtable.btableSchema
-    val schemaFields: Array[StructField] = relationSchema.fields
-    val schemaFieldNames: Array[String] = schemaFields.map(_.name)
+    val relationBtable: SqlBindingTableMetadata =
+      relation.bindingTable.asInstanceOf[SqlBindingTableMetadata]
+    val relationSchemaMap: Map[Reference, StructType] = relationBtable.schemaMap
 
-    val existingColumns: Seq[Reference] =
-      attributes.filter(attr => schemaFieldNames.exists(_.startsWith(attr.refName)))
-    val existingColumnsSchemaFields: Map[Reference, Array[StructField]] =
-      existingColumns
-        .map(attr => attr -> schemaFields.filter(_.name.startsWith(attr.refName)))
-        .toMap
-    val existingColumnsSchemaString: Iterable[String] =
-      existingColumnsSchemaFields
-        .values
-        .flatten
+    val existingRefs: Seq[Reference] =
+      attributes.filter(attr => relationSchemaMap.get(attr).isDefined)
+    val existingColumnsSelect: Set[String] =
+      existingRefs
+        .flatMap(ref => relationSchemaMap(ref).fields)
         .map(field => s"`${field.name}`")
+        .toSet
 
-    val newColumns: Set[Reference] =
-      attributes.filter(attr => !schemaFieldNames.exists(_.startsWith(attr.refName))).toSet
-    val newColumnsSchemaFields: Map[Reference, Array[StructField]] =
-      newColumns
-        .map(ref =>
-          ref -> Array(
-            StructField(
-              name = s"${ref.refName}$$${idColumn.columnName}",
-              dataType = IntegerType)))
-        .toMap
-    val newColumnsSchemaString: Set[String] =
-      newColumns.map(ref => s"`${ref.refName}$$${idColumn.columnName}`")
+    val refsNotInSchema: Set[Reference] =
+      attributes
+        .filter(attr => relationSchemaMap.get(attr).isEmpty)
+        .toSet
+    val refsInSchemaWithoutId: Set[Reference] =
+      existingRefs // in schema
+        .filterNot(ref =>
+          relationSchemaMap(ref)
+            .fields
+            .exists(_.name == s"${ref.refName}$$${idColumn.columnName}")) // no id column
+        .toSet
+    val newRefs: Set[Reference] = refsNotInSchema ++ refsInSchemaWithoutId
+    // For each new variable, we add a column containing monotonically increasing id's. We cannot
+    // simply add any constant here, because it will be coalesced into a single group by a GROUP BY
+    // clause. The monotonic id is actually one id per partition and, for each row on that partition
+    // it increases by one. Therefore, in the resulting table, we may see very skewed id's in the
+    // new columns. However, they should be distinct from each other and thus have all the new rows
+    // preserved after the GROUP BY.
+    val newRefsSelect: Set[String] =
+      newRefs.map(
+        ref => s"MONOTONICALLY_INCREASING_ID() AS `${ref.refName}$$${idColumn.columnName}`")
 
-    val columnSelectionList: String =
-      (existingColumnsSchemaString ++ newColumnsSchemaString).mkString(", ")
+    val columnsToSelect: Set[String] = existingColumnsSelect ++ newRefsSelect
 
-    val sqlProject: String = {
-      val table: String = {
-        if (newColumns.nonEmpty) {
-          // For each new variable, we add a column containing monotonically increasing id's. We
-          // cannot simply add any constant here, because it will be coalesced into a single group
-          // by a GROUP BY clause. The monotonic id is actually one id per partition and, for each
-          // row on that partition it increases by one. Therefore, in the resulting table, we may
-          // see very skewed id's in the new columns. However, they should be distinct from each
-          // other and thus have all the new rows preserved after the GROUP BY.
-          val selectNewVarsId: String =
-            newColumns
-              .map(
-                ref => s"MONOTONICALLY_INCREASING_ID() AS `${ref.refName}$$${idColumn.columnName}`")
-              .mkString(", ")
-
-          s"""
-          SELECT *, $selectNewVarsId FROM (${relationBtable.btable.resQuery})"""
-
-        } else
-          relationBtable.btableOps.resQuery
-      }
-
+    val sqlProject: String =
       s"""
-      SELECT $columnSelectionList FROM ($table)"""
-    }
+      SELECT ${columnsToSelect.mkString(", ")} FROM (${relationBtable.btable.resQuery})"""
 
-    val projectionSchema: StructType =
-      StructType(
-        (existingColumnsSchemaFields.values.flatten ++ newColumnsSchemaFields.values.flatten)
-          .toArray)
+    val refsInSchemaWithIdRefSchema: Map[Reference, StructType] =
+      existingRefs
+        .filterNot(ref => refsInSchemaWithoutId.contains(ref))
+        .map(ref => ref -> relationSchemaMap(ref))
+        .toMap
+    val refsInSchemaWithoutIdRefSchema: Map[Reference, StructType] =
+      refsInSchemaWithoutId
+        .map(ref => {
+          val previousSchema: StructType = relationSchemaMap(ref)
+          val newIdStructField: StructField =
+            StructField(s"${ref.refName}$$${idColumn.columnName}", IntegerType)
+          ref -> StructType(previousSchema.fields :+ newIdStructField)
+        })
+        .toMap
+    val refsNotInSchemaRefSchema: Map[Reference, StructType] =
+      refsNotInSchema
+        .map(ref =>
+          ref -> StructType(Array(
+            StructField(s"${ref.refName}$$${idColumn.columnName}", IntegerType))))
+        .toMap
+
     val projectionRefSchema: Map[Reference, StructType] =
-      existingColumnsSchemaFields.mapValues(StructType(_)) ++
-        newColumnsSchemaFields.mapValues(StructType(_))
+      refsInSchemaWithIdRefSchema ++ refsInSchemaWithoutIdRefSchema ++ refsNotInSchemaRefSchema
+    val projectionSchema: StructType =
+      StructType(projectionRefSchema.values.flatMap(_.fields).toArray)
 
     SqlBindingTableMetadata(
       sparkSchemaMap = projectionRefSchema,
